c655cda6c6d807c93c687747d09298b14b09bb40
Mark Lamourine <markllama@gmail.com>
2013-02-14 16:43:31 -0800

    strips() works

--- powerusb.py
+++ powerusb.py
@@ -26,9 +26,15 @@ import argparse
 import re
 import usb
 
+##############################################################################
+#
+# Argument Parsing
+#
+##############################################################################
+
 class CommandAction(argparse.Action):
     """
-    
+    Parse one of the 5 command actions
     """
     def __call__(self, parser, namespace, values, option_string=None):
         namespace.command = re.sub("^--", "", option_string)
@@ -57,8 +63,59 @@ def parse_command_line():
     parser.add_argument("--reset", action="store_true")
     return parser.parse_args()
 
+###############################################################################
+#
+# HID Device Library
+#
+###############################################################################
+
+
+###############################################################################
+#
+# PowerUSB Objects
+#
+###############################################################################
+
+class PowerUSBStrip():
+    """
+    A PowerUSB switched power strip
+    """
+
+    _vendor_id = 0x04d8
+    _product_id = 0x003f
+
+    def __init__(self, usb_device):
+        self.usb_device = usb_device
+
+    @staticmethod
+    def strips():
+        """
+        Return the set of connected power strips
+        """
+        return [PowerUSBStrip(d) for b in usb.busses() for d in b.devices
+                if d.idVendor == PowerUSBStrip._vendor_id 
+                and d.idProduct == PowerUSBStrip._product_id]
+
+
+###############################################################################
+#
+# PowerUSB Commands
+#
+###############################################################################
+
+def strips():
+    print PowerUSBStrip.strips()
+
+###############################################################################
+#
+# MAIN
+#
+###############################################################################
 if __name__ == "__main__":
 
     opts = parse_command_line()
+
+    if opts.strips:
+        strips()
 
     print opts
