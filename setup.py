987b914c3b5b1a3c5f7aec0e3d7f0e0012c4519b
Mark Lamourine <markllama@gmail.com>
2013-03-15 18:59:02 -0700

    Automatic commit of package [powerusb] release [1.3-1].

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="powerusb",
-      version="1.2",
+      version="1.3",
       description="Control PowerUSB power strips",
       long_description="""
 Library and CLI tools to Control PowerUSB power strips.
